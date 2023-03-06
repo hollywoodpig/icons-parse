ff83dc786128a24a1def8a28a5ba85c39f6224d6
Nikita Kirsha <NekitKLD@gmail.com>
2023-03-06 17:11:50 +0200

    another refactor

--- helpers.js
+++ helpers.js
@@ -1,5 +1,56 @@
+import translate from 'translate';
+import path from 'path';
+import glob from 'glob';
+import replace from 'replace-in-file';
+import fs from 'fs/promises';
+
 export function* chunkify(arr, n) {
 	for (let i = 0; i < arr.length; i += n) {
 		yield arr.slice(i, i + n);
 	}
+}
+
+export async function translateText(query) {
+	const translated = await translate(query, {
+		from: 'en',
+		to: 'ru',
+		cache: 100,
+	});
+
+	return translated;
+}
+
+export async function translateArray(arr, mock) {
+	const chunks = [...chunkify(arr, 10)];
+	const translatedChunks = [];
+
+	for (const chunk of chunks) {
+		const text = chunk.join('; ');
+		const translatedText = mock
+			? text.replace(/\w+/g, 'translated')
+			: await translateText(text);
+		const translatedChunk = translatedText.split('; ');
+
+		translatedChunks.push(translatedChunk);
+	}
+
+	return translatedChunks.flat();
+}
+
+export async function formatIcons(name) {
+	const files = await glob(`packs/${name}/svg/*.svg`);
+
+	await fs.mkdir(`dest/${name}`, { recursive: true });
+
+	for (const file of files) {
+		const basename = path.basename(file);
+
+		await fs.copyFile(file, `dest/${name}/${basename}`);
+	}
+
+	await replace({
+		files: `dest/${name}/*.svg`,
+		from: '<svg',
+		to: '<svg id="icon"',
+	});
 }
